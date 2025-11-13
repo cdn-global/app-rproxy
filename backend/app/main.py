00c490e6af78aaf1023a0f51a10be5805e670182
cdn <nik@cobaltdata.net>
2025-11-13 14:14:14 +0000

    core

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,9 +23,7 @@ app = FastAPI(
 if settings.all_cors_origins:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            str(origin).strip("/") for origin in settings.all_cors_origins
-        ],
+        allow_origins=settings.all_cors_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
