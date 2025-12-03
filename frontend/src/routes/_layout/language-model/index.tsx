f18d96f7e372babfc57e9dd5ae05db5bd6ffbefe
cdn <nik@cobaltdata.net>
2025-12-03 10:45:51 +0000

    core

--- frontend/src/routes/_layout/language-model/index.tsx
+++
@@ -1,6 +0,0 @@
-import LanguageModel from '@/components/LanguageModel/Index'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_layout/language-model/')({
-  component: LanguageModel,
-})
